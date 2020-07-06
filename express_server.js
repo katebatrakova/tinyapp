764abeeccdde1c7bc93907a7f55556fd624861c4
katebatrakova <kateryna.batrakova@gmail.com>
2020-07-06 17:45:47 -0400

    Add additional endpoints/routes

--- express_server.js
+++ express_server.js
@@ -6,11 +6,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+////add  endpoint
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+//add additional endpoints
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
