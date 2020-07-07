b1a55f69d25929d36dd4014eef31d2017067921d
katebatrakova <kateryna.batrakova@gmail.com>
2020-07-06 20:25:46 -0400

    Add shortURL routeand template in views

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 ////add  endpoint
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -37,6 +38,13 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
