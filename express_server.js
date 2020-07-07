cc22035882ea6038af5ac534ec7ef0abe605e404
katebatrakova <kateryna.batrakova@gmail.com>
2020-07-06 22:00:34 -0400

    Create a  Form , Add a POST Route, add generateRandomString

--- express_server.js
+++ express_server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+//body-parser library converts the request body from a Buffer into string
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 //Set ejs as the view engine
 app.set("view engine", "ejs")
 
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(6);
+}
+
+
 const urlDatabase = {
-  'b2xVn2': "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   '9sm5xK': "http://www.google.com"
 };
 
 
+
+//handling the POST request
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 ////add  endpoint
 app.get("/", (req, res) => {
@@ -41,10 +57,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
 
 
 app.listen(PORT, () => {
