86b300f34515df516f1c27559bd5d0e19a5c2d56
katebatrakova <kateryna.batrakova@gmail.com>
2020-07-07 20:07:17 -0400

    Set up cookie for username initial

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require('cookie-parser')
 
 //Set ejs as the view engine
 app.set("view engine", "ejs")
@@ -8,6 +9,7 @@ app.set("view engine", "ejs")
 //body-parser library converts the request body from a POST request Buffer into string
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 function generateRandomString() {
   return Math.random().toString(36).substr(6);
@@ -50,7 +52,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 
-//Page http://localhost:8080/urls/9sm5xK
+//Page http://localhost:8080/urls/:shortURL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -61,15 +63,17 @@ app.post("/urls/:shortURL/", (req, res) => {
   const shortURL = req.params.shortURL;
   const updatedURL = req.body.longURL;
   urlDatabase[shortURL] = updatedURL;
-  console.log(urlDatabase); //updated urlDatabase
+  // console.log(urlDatabase); //updated urlDatabase
   res.redirect('/urls');
 });
 
-//EDIT redirect
-app.get("/urls/:shortURL", (req, res) => {
-  console.log('edit buton redirect clicked');
-  res.render("urls_show");
-});
+//LOGIN
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  console.log(req.cookies, ' cookies');
+  res.redirect('/urls');
+})
 
 
 app.listen(PORT, () => {
